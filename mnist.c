93b6864acb990bc9f0be9adc8fda37dc4b407e6b
Abigail Adegbiji <adegbijiabigail@gmail.com>
2023-08-08 21:28:36 -0600

    thinking about the neural network training algorithm

--- mnist.c
+++ mnist.c
@@ -4,46 +4,13 @@
 
 typedef unsigned char u8;
 
-class MNIST {
-public:
-    MNIST();
-    ~MNIST();
-private:
-    const int img_row  = 28;
-    const int img_col  = 28;
-    const int train_count = 60000;
-    const int test_count  = 10000;
-    const int img_size = img_row * img_col;
+const int img_row  = 28;
+const int img_col  = 28;
+const int train_count = 60000;
+const int test_count  = 10000;
+const int img_size = img_row * img_col;
 
-    u8 *test_images;
-    u8 *train_images;
-    int *test_labels;
-    int *train_labels;
-
-    void load_labels(int *labels, int count, const char *file);
-    void load_images(unsigned char *images, int count, const char *file);
-};
-
-MNIST::MNIST() {
-    test_labels = (int*) malloc(test_count * sizeof(int));
-    test_images = (u8*) malloc(test_count * img_size * sizeof(u8));
-    load_images(test_images, test_count, "data/testing.idx3-ubyte");
-    load_labels(test_labels, test_count, "data/testing-labels.idx1-ubyte");
-
-    train_labels = (int*) malloc(train_count * sizeof(int));
-    train_images = (u8*) malloc(train_count * img_size * sizeof(u8));
-    load_images(train_images, train_count, "data/training.idx3-ubyte");
-    load_labels(train_labels, train_count, "data/training-labels.idx1-ubyte");
-}
-
-MNIST::~MNIST() {
-    free(test_labels);
-    free(test_images);
-    free(train_labels);
-    free(train_images);
-}
-
-void MNIST::load_labels(int *labels, int count, const char *file) {
+void load_labels(int *labels, int count, const char *file) {
     FILE *fp = fopen(file, "rb");
 
     u8 size[4];
@@ -63,7 +30,7 @@ void MNIST::load_labels(int *labels, int count, const char *file) {
     fclose(fp);
 }
 
-void MNIST::load_images(unsigned char *images, int count, const char *file) {
+void load_images(unsigned char *images, int count, const char *file) {
     FILE *fp = fopen(file, "rb");
 
     u8 size[4];
@@ -93,7 +60,20 @@ void MNIST::load_images(unsigned char *images, int count, const char *file) {
 }
 
 int main() {
-    MNIST nn;
+    int *test_labels = (int*) malloc(test_count * sizeof(int));
+    u8 *test_images = (u8*) malloc(test_count * img_size * sizeof(u8));
+    load_images(test_images, test_count, "data/testing.idx3-ubyte");
+    load_labels(test_labels, test_count, "data/testing-labels.idx1-ubyte");
+
+    int *train_labels = (int*) malloc(train_count * sizeof(int));
+    u8 *train_images = (u8*) malloc(train_count * img_size * sizeof(u8));
+    load_images(train_images, train_count, "data/training.idx3-ubyte");
+    load_labels(train_labels, train_count, "data/training-labels.idx1-ubyte");
+
+    free(test_labels);
+    free(test_images);
+    free(train_labels);
+    free(train_images);
 }
 
 /*
@@ -102,20 +82,23 @@ Input layer: 784 neurons
 Hidden layer: 100 neurons
 Output layer: 10 neurons
 
+The dot product between 100x784 and 784x1 results in 100x1. Then we can just add the bias, which happens to be 100x1!
+The dot product between 10x100 and 100x1 results in 10x1. Then we can just add the bias, which happens to be 10x1!
+
 Forward propagation:
-hidden_layer           = hidden_weights * input_layer + hidden_bias
+hidden_layer           = dot(hidden_weights, input_layer) + hidden_bias
 activated_hidden_layer = sigmoid(hidden_layer)
-output_layer           = output_weights * activated_hidden_layer + output_bias
+output_layer           = dot(output_weights, activated_hidden_layer) + output_bias
 activated_output_layer = softmax(output_layer)
 
 Backward propagation:
 loss = mean_squared_error(target_output, activated_output_layer)
 
 output_gradient         = activated_output_layer - target_output
-output_weights_gradient = output_gradient * activated_hidden_layer
+output_weights_gradient = dot(output_gradient, transpose(activated_hidden_layer))
 
-hidden_gradient         = (transpose(output_weights) * output_gradient) * (activated_hidden_layer * (1 - activated_hidden_layer))
-hidden_weights_gradient = hidden_gradient * input_layer
+hidden_gradient         = dot(transpose(output_weights), output_gradient) * (activated_hidden_layer * (1 - activated_hidden_layer))
+hidden_weights_gradient = dot(hidden_gradient, transpose(input_layer))
 
 output_weights -= learning_rate * output_weights_gradient
 output_bias    -= learning_rate * output_gradient
